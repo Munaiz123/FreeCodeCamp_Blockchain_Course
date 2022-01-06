ec0a4ad2df78d85a694b07c9c230edbf43dd5224
Munaiz <munaizahmed2@gmail.com>
2022-01-05 20:13:42 -0600

    Lesson 4 cont. - downloaded ganache-cli. updated based on ganache cli --deterministic tag. Next: connect to remote testNet via infura or alchemy

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -35,9 +35,9 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # Local simulated blockchain -> Ganache (it'll be our Javascript VM for our local machine)
 # Connecting to Ganache
 
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
 chain_id = 1337
-my_address = "0x7eeB11bEa83b3ECB18911E82A4689278286D8171"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 private_key = os.getenv("PRIVATE_KEY") # access private key by 'export PRIVATE_KEY=....' from terminal
 
 # Create contract in python
