49eb9fd8351156666470be66cd92bd398c410dd4
Munaiz <munaizahmed2@gmail.com>
2022-01-11 20:11:50 -0600

    Lesson 5 cont. - created brownie-config.yml where we were able to pull in our wallet keys into our deploy scripts

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,11 +1,18 @@
 
-from brownie import accounts
+from brownie import accounts, config
+# import os
 
 def deploy_simple_storage():
-  account = accounts[0]
-  # print(account)
-  myAccount = accounts.load('freecodecamp-account')
-  print(myAccount)
+  account = accounts[0] # USING BROWNIE'S BUILT IN GANACHE ACCOUNTS THAT IT RUNS UP UPON INVOKATION
+  print(account)
+
+  # myAccount = accounts.load('munziTest') # ADDING ACCOUNTS PRIVATE KES THROUGH ECNCRYPTED COMMAND LINES
+
+  # myAccount = accounts.add(os.getenv("PRIVATE_KEY"))  # USES .ENV  FILE
+
+  # myAccount = accounts.add(config["wallets"]["from_key"]) USING brownie-config.yml to get directly from .env file -> don't need to import OS but need import 'config' from brownie
+  # print(myAccount)
+
 
 
 def main():
