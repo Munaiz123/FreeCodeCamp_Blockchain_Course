a165b921b7df64a6a53c12d3f4e96e4598378df6
Munaiz <munaizahmed2@gmail.com>
2022-01-12 22:16:17 -0600

    Lesson 5 cont. - Added Alchemy network to brownie. Deployed SimpleStorageContract using Alchemy network via brownie CLI. Added get_account() in deploy.py

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,14 +1,14 @@
 
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 # import os
 
 def deploy_simple_storage():
   print("*****************************************************************************************")
-  account = accounts[0] # USING BROWNIE'S BUILT IN GANACHE ACCOUNTS THAT IT RUNS UP UPON INVOKATION
+  account = get_account()
   print("Deploying....")
   simple_storage = SimpleStorage.deploy({"from":account}) # always need a "from" key when making a transaction
 
-
+  print("Storing....")
   transaction =  simple_storage.store(17,{"from":account})
   transaction.wait(1) # wait for transactions to finish
 
@@ -17,12 +17,15 @@ def deploy_simple_storage():
   print("stored_value =>", stored_value)
 
 
-  # myAccount = accounts.load('munziTest') # ADDING ACCOUNTS PRIVATE KES THROUGH ECNCRYPTED COMMAND LINES
+def get_account():
+  if(network.show_active() == 'development'):
+    return accounts[0]  # USING BROWNIE'S BUILT IN GANACHE ACCOUNTS THAT IT RUNS UP UPON INVOKATION
+  else:
+    return accounts.add(config["wallets"]["from_key"]) # USING brownie-config.yml to get directly from .env file -> don't need to import OS but need import 'config' from brownie
+    # return accounts.load('munziTest') # ADDING ACCOUNTS PRIVATE KES THROUGH ECNCRYPTED COMMAND LINES
+    # return accounts.add(os.getenv("PRIVATE_KEY"))  # USES .ENV  FILE
 
-  # myAccount = accounts.add(os.getenv("PRIVATE_KEY"))  # USES .ENV  FILE
 
-  # myAccount = accounts.add(config["wallets"]["from_key"]) USING brownie-config.yml to get directly from .env file -> don't need to import OS but need import 'config' from brownie
-  # print(myAccount)
 
 
 
